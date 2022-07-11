ba4e9e6d86b5292ae380f995ea640bbe2ee36259
kiwoon kim <kisoon0947@gmail.com>
2022-07-11 17:04:22 +0900

    update

--- basics.ts
+++ basics.ts
@@ -12,3 +12,25 @@ userName = 'kiwoon'
 
 let isInstructor: boolean
 isInstructor = true
+
+// More complex types
+
+let hobbies: string[]
+hobbies = ['Sports', 'Cooking']
+
+let person: {
+  name: string
+  age: number
+  isEmployee: boolean
+}
+
+person = {
+  name: 'Kiwoon',
+  age: 36,
+  isEmployee: true,
+}
+
+let people: {
+  name: string
+  age: number
+}[]
