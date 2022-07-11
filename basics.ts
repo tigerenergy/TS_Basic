7c723bf028f9770c6677af3b527a7c72021511bf
kiwoon kim <kisoon0947@gmail.com>
2022-07-11 16:48:58 +0900

    update

--- basics.ts
+++ basics.ts
@@ -1,3 +1,14 @@
 // Primitives : number, string, boolean
 // More complex types: array, object
 // Function types, parameters
+
+// Primitives
+
+let age: number
+age = 12
+
+let userName: string
+userName = 'kiwoon'
+
+let isInstructor: boolean
+isInstructor = true
